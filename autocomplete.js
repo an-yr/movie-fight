6d7aa3335025a89b6e72cba43662238035f8350b
Angela Yuri <ang.saito@gmail.com>
2022-02-21 22:01:38 -0200

    displayed multiple autocomplete

---
+++ autocomplete.js
@@ -0,0 +1,57 @@
+const createAutoComplete = ({ root }) => {
+  root.innerHTML = `
+    <label><b>Search for a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+`;
+
+  const input = root.querySelector("input");
+  const dropdown = root.querySelector(".dropdown");
+  const resultsWrapper = root.querySelector(".results");
+
+  // action that happens when receive a input
+  const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+
+    if (!movies.length) {
+      dropdown.classList.remove("is-active");
+      return;
+    }
+
+    resultsWrapper.innerHTML = "";
+    // fatchData happens, then dropdown is active
+    dropdown.classList.add("is-active");
+
+    for (let movie of movies) {
+      const option = document.createElement("a");
+      const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+      option.classList.add("dropdown-item");
+      option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+    `;
+      option.addEventListener("click", () => {
+        dropdown.classList.remove("is-active");
+        input.value = movie.Title;
+        onMovieSelect(movie);
+      });
+
+      resultsWrapper.appendChild(option);
+    }
+  };
+
+  //debounce function wrapping the function
+  input.addEventListener("input", debounce(onInput, 500));
+
+  // click happens outside of the root, close the dropdown
+  document.addEventListener("click", (event) => {
+    if (!root.contains(event.target)) {
+      dropdown.classList.remove("is-active");
+    }
+  });
+};
--- index.js
+++ index.js
@@ -10,64 +10,15 @@ const fetchData = async (movieSearched) => {
   }
   return response.data.Search;
 };
-
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-  <label><b>Search for a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-// action that happens when receive a input
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-
-  resultsWrapper.innerHTML = "";
-  // fatchData happens, then dropdown is active
-  dropdown.classList.add("is-active");
-
-  for (let movie of movies) {
-    const option = document.createElement("a");
-    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-
-    option.classList.add("dropdown-item");
-    option.innerHTML = `
-      <img src="${imgSrc}" />
-      ${movie.Title}
-    `;
-    option.addEventListener("click", () => {
-      dropdown.classList.remove("is-active");
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    });
-
-    resultsWrapper.appendChild(option);
-  }
-};
-
-//debounce function wrapping the function
-input.addEventListener("input", debounce(onInput, 500));
-
-// click happens outside of the root, close the dropdown
-document.addEventListener("click", (event) => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove("is-active");
-  }
+createAutoComplete({
+  root: document.querySelector(".autocomplete"),
 });
-
+createAutoComplete({
+  root: document.querySelector(".autocomplete-two"),
+});
+createAutoComplete({
+  root: document.querySelector(".autocomplete-three"),
+});
 // get more info of the selected movie
 const onMovieSelect = async (movie) => {
   const response = await axios.get("http://www.omdbapi.com/", {
